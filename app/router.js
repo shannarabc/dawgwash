83bfcb7bd10bb926a56934d24daee703fb6cd611
shannarabc <shannarabc@gmail.com>
2017-05-23 18:39:05 -0500

    started new design, added all pages.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('services');
+  this.route('contact');
+  this.route('faqs');
+  this.route('about');
 });
 
 export default Router;
